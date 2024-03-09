6f56107c7604e0267aee50be4c1266533f20e663
abc-hi <revathimohancse@gmail.com>
2024-03-09 00:01:03 -0800

    3rd update

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.get('/', (req, res) => {
     const endpoints = `
         <h2>API Endpoints:</h2>
         <ul>
-            <li>API Endpoint for list all rooms: <strong>GET</strong> <a href="http://localhost:4000/api/get-room">http://localhost:4000/api/get-room</a></li>
-            <li>API Endpoint for all rooms with booked details: <strong>GET</strong> <a href="http://localhost:4000/api/booked-room">http://localhost:4000/api/booked-room</a></li>
-            <li>API Endpoint for list all customers with booked data: <strong>GET</strong> <a href="http://localhost:4000/api/get-customerdata">http://localhost:4000/api/get-customerdata</a></li>
-            <li>API Endpoint for how many customers have booked: <strong>GET</strong> <a href="http://localhost:4000/api/book-count">http://localhost:4000/api/book-count</a></li>
-            <li>API Endpoint for creating a new room: <strong>POST</strong> <a href="http://localhost:4000/api/create-room">http://localhost:4000/api/create-room</a></li>
-            <li>API Endpoint for booking room: <strong>POST</strong> <a href="http://localhost:4000/api/book-room">http://localhost:4000/api/book-room</a></li>
+            <li>API Endpoint for list all rooms: <strong>GET</strong>  <p style={{color: 'green'}}>http://localhost:4000/api/get-room</p></li>
+            <li>API Endpoint for all rooms with booked details: <strong>GET</strong>  <p style={{color: 'green'}}>http://localhost:4000/api/booked-room</p></li>
+            <li>API Endpoint for list all customers with booked data: <strong>GET</strong> <p style={{color: 'green'}}> http://localhost:4000/api/get-customerdata</p></li>
+            <li>API Endpoint for how many customers have booked: <strong>GET</strong>  <p style={{color: 'green'}}>http://localhost:4000/api/book-count</p></li>
+            <li>API Endpoint for creating a new room: <strong>POST</strong>  <p style={{color: 'green'}}>http://localhost:4000/api/create-room</p></li>
+            <li>API Endpoint for booking room: <strong>POST</strong> <p style={{color: 'green'}}>http://localhost:4000/api/book-room</p></li>
         </ul>
     `;
     res.send(endpoints);});    
