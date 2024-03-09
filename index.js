5c8bb001988b02354ae9875035afb7dc47c0049e
abc-hi <revathimohancse@gmail.com>
2024-03-08 22:00:05 -0800

    3rd update

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const hallrouter = require("./Routers/hallbook-router.js")
 
 const app =express();
 const PORT=4000;
-app.get('/',(req,res)=>{
+
     
-    res.send(`API Endpoint for list all rooms: GET http://localhost:4000/api/get-room`,
-    `API Endpoint for all rooms with booked details : GET http://localhost:4000/api/booked-room`,
-    `API Endpoint for list all customers with booked data: GET http://localhost:4000/api/get-customerdata`,
-    `API Endpoint for how many customers have booked: GET http://localhost:4000/api/book-count`,
-    `API Endpoint for creating a new room: POST http://localhost:4000/api/create-room`,
-    `API Endpoint for booking room : POST http://localhost:4000/api/book-room`);});
+    app.get('/', (req, res) => {
+        const endpoints = `
+            API Endpoint for list all rooms: GET http://localhost:4000/api/get-room
+            API Endpoint for all rooms with booked details: GET http://localhost:4000/api/booked-room
+            API Endpoint for list all customers with booked data: GET http://localhost:4000/api/get-customerdata
+            API Endpoint for how many customers have booked: GET http://localhost:4000/api/book-count
+            API Endpoint for creating a new room: POST http://localhost:4000/api/create-room
+            API Endpoint for booking room: POST http://localhost:4000/api/book-room
+        `;
+        res.send(endpoints);
+    });
 
     
 
